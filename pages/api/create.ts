ba36f88923c91d88ec42ad7d00789fc98980733f
Atshilla Fatih Ibrahim <shilla.ibra@gmail.com>
2022-11-14 13:50:33 +0700

    first commit

---
+++ pages/api/create.ts
@@ -0,0 +1,17 @@
+import { prisma } from "../../lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const {title, content} = req.body
+    try {
+        await prisma.note.create({
+            data: {
+                title,
+                content
+            }
+        })
+        res.status(200).json({message: 'Success'})
+    } catch(err){
+        console.log("fail")
+    }
+}
